67e7894a7df74f92f02e2cd0a7116ae6e61be31b
HE\He <szhgoldeneye@hotmail.com>
2017-05-08 18:16:02 +0800

    add config

--- php/executor.php
+++ php/executor.php
@@ -1,15 +1,17 @@
 <?php
+$config = parse_ini_file('config.ini');
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+if($config["debug"]){
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-$con = socket_connect($socket, '127.0.0.1', 12315);
+$con = socket_connect($socket, $config["host"], $config["port"]);
 if(!$con) {
     socket_close($socket);
     exit;
 }
-
 if($con){
     $cmdl = isset($_GET['cmdl']) ? $_GET['cmdl'] : "whoami";
     socket_write($socket, $cmdl . "\r\n\r\n");
@@ -21,7 +23,6 @@ if($con){
     $hear = str_replace(PHP_EOL, '<br/>', $hear);
     echo $hear;
 }
-
 socket_shutdown($socket);
 socket_close($socket);
 ?>
